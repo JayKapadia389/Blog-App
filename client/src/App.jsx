a19ae97bb62adaf116b24d3f8166a6c7a903cafd
Jay <jaykapadia389@gmail.com>
2023-08-14 00:42:25 +0530

    home page restructure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,14 +4,25 @@ function App() {
 
   return (
     <div id="home-bg-image-container">
+
+      <header id='header'>
+      </header>
+
+      <div id="signup-login-header">
+          <form id="signup-login-wrap">
+                <button className="signup-login signup" >SignUp</button>
+                <button className="signup-login login" >Login</button>
+          </form>
+      </div>
+
       <div id="home-trans-container">
         <div id="home-blogspot-wrap">
           <h1 id="blogspot-h1" className="blogspot">blogspot</h1>
           <h2 id="sub-title">read, write and explore</h2>
-          <form id="signup-login-wrap-main">
-            <button className="signup-login-main signup" >SignUp</button>
-            <button className="signup-login-main login" >Login</button>
-          </form>
+        </div>
+
+        <div id="carousel-container">
+
         </div>
       </div>
     </div>

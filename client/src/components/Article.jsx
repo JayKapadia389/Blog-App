b348454670c9b1cc6a3764f5b9cf0381ead42ace
Jay <jaykapadia389@gmail.com>
2023-08-31 23:21:14 +0530

    completed article page ui

--- client/src/components/Article.jsx
+++ client/src/components/Article.jsx
@@ -1,11 +1,163 @@
+import { AiOutlineHeart } from "react-icons/ai"
+import { AiFillHeart } from "react-icons/ai"
+import { PiShareNetworkBold } from "react-icons/pi"
+import { BsBookmark } from "react-icons/bs"
+import { BsBookmarkFill } from "react-icons/bs"
+import {useState} from "react";
+
 function Article(props){
 
+    let [liked,setLiked] = useState(false); 
+    let [saved,setSaved] = useState(false); 
+
     return(
-        <div>
+        <main id="article-component">
             
-            article
+            <div id="article-tbnl-div">
+                <img id="article-tbnl" src="/src/assets/react.webp"></img>
+            </div>
+
+            <h1 id="article-title">5 React useState Mistakes That Will Get You Fired</h1>
+
+            <div id="article-body">
+
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae, id minus nesciunt perferendis corrupti exercitationem quis iste nobis quos esse consequuntur et doloremque inventore repellendus odio quia architecto ex similique.
+                <br/>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id exercitationem corporis consectetur, perferendis tempora nemo doloremque excepturi minima perspiciatis libero sed. Hic corporis, odio ex ducimus officia eaque. Quod, minus quo. Et cum minima alias distinctio laboriosam nemo inventore ut id mollitia quos non molestias soluta blanditiis, reiciendis repudiandae quisquam laudantium quasi doloribus molestiae perspiciatis voluptas beatae consequuntur sunt voluptates! Sit, vel! At iste consequuntur, consequatur laborum dolor culpa cum vero distinctio cumque maxime doloribus nihil soluta corporis praesentium perferendis voluptas aliquid tenetur optio commodi nisi ad. Alias expedita aliquam nesciunt. Magnam officia, repellat nulla error dolorem fuga tenetur voluptatibus?
+                <br/>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem excepturi sint pariatur, itaque officiis illum unde tempore repellat, aut natus cumque eius mollitia doloremque quaerat ex asperiores cum perspiciatis maxime repudiandae qui odio. Id quidem, rem recusandae unde dolores fugit eum odit quas nisi praesentium obcaecati consectetur! Est, optio possimus quasi ullam autem quisquam nemo magni temporibus! Blanditiis, aut ducimus!
+                <br/>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, alias modi nemo sed saepe facilis quae quas laudantium voluptates expedita.
+                <br/>
+
+            </div>
+
+            <div id="interaction-bar">
+                <span>
+                    { liked ? <AiFillHeart onClick={()=>{ setLiked(!liked)}}/> : <AiOutlineHeart onClick={()=>{ setLiked(!liked)}}/>}
+                </span>
+
+                <span className="empty">
+                    
+                </span>
+
+                <span>
+                    <PiShareNetworkBold/>
+                </span>
+
+                <span>
+                     { saved ? <BsBookmarkFill onClick={()=>{ setSaved(!saved)}}/> : <BsBookmark onClick={()=>{ setSaved(!saved)}}/>}  
+                </span>
+
+            </div>
+
+            <div id="comment-section">
+
+                <input type="text" 
+                       id="comment-input"
+                       placeholder="Write a comment..."/>
+
+                <h3>Comments</h3>
+
+                <div id="all-comments">
+
+                <div className="comment-wrap">
+
+                    <div className="comment-profile">
+
+                        <div className="comment-profile-pic-div">
+                            <img className="comment-profile-pic" src="/src/assets/jay.jpg"></img>
+                        </div>
+
+                        <div >
+                            <p className="comment-author">jay kapadia</p>
+                            <p className="comment-time">2 days ago</p>
+                        </div>
+
+                    </div>
+
+                    <p className="comments">Actually, I also use presentational and containers Pattern for UI re-usability in NextJS rather than traditional components. </p>
+
+                    <span>
+                        { liked ? <AiFillHeart onClick={()=>{ setLiked(!liked)}}/> : <AiOutlineHeart onClick={()=>{ setLiked(!liked)}}/>}
+                    </span>
+
+                </div>
+
+                <div className="comment-wrap">
+
+                    <div className="comment-profile">
+
+                        <div className="comment-profile-pic-div">
+                            <img className="comment-profile-pic" src="/src/assets/jay.jpg"></img>
+                        </div>
+
+                        <div >
+                            <p className="comment-author">jay kapadia</p>
+                            <p className="comment-time">2 days ago</p>
+                        </div>
+
+                    </div>
+
+                    <p className="comments">Actually, I also use presentational and containers Pattern for UI re-usability in NextJS rather than traditional components. </p>
+
+                    <span>
+                        { liked ? <AiFillHeart onClick={()=>{ setLiked(!liked)}}/> : <AiOutlineHeart onClick={()=>{ setLiked(!liked)}}/>}
+                    </span>
+
+                </div>
+
+                <div className="comment-wrap">
+
+                    <div className="comment-profile">
+
+                        <div className="comment-profile-pic-div">
+                            <img className="comment-profile-pic" src="/src/assets/jay.jpg"></img>
+                        </div>
+
+                        <div >
+                            <p className="comment-author">jay kapadia</p>
+                            <p className="comment-time">2 days ago</p>
+                        </div>
+
+                    </div>
+
+                    <p className="comments">Actually, I also use presentational and containers Pattern for UI re-usability in NextJS rather than traditional components. </p>
+
+                    <span>
+                        { liked ? <AiFillHeart onClick={()=>{ setLiked(!liked)}}/> : <AiOutlineHeart onClick={()=>{ setLiked(!liked)}}/>}
+                    </span>
+
+                </div>
+
+                <div className="comment-wrap">
+
+                    <div className="comment-profile">
+
+                        <div className="comment-profile-pic-div">
+                            <img className="comment-profile-pic" src="/src/assets/jay.jpg"></img>
+                        </div>
+
+                        <div >
+                            <p className="comment-author">jay kapadia</p>
+                            <p className="comment-time">2 days ago</p>
+                        </div>
+
+                    </div>
+
+                    <p className="comments">Actually, I also use presentational and containers Pattern for UI re-usability in NextJS rather than traditional components. </p>
+
+                    <span>
+                        { liked ? <AiFillHeart onClick={()=>{ setLiked(!liked)}}/> : <AiOutlineHeart onClick={()=>{ setLiked(!liked)}}/>}
+                    </span>
+
+                </div> 
+
+                </div>      
+
+            </div>
             
-        </div>
+        </main>
     )
 }
 

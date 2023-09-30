69c3ed4d604e235d9d4903e193a5033de6306e33
Jay <jaykapadia389@gmail.com>
2023-10-01 00:47:20 +0530

    edit profile frontend

--- client/src/components/Editprofile.jsx
+++ client/src/components/Editprofile.jsx
@@ -3,10 +3,13 @@ import axios from 'axios';
 import { be_url } from '/config'; 
 import { useNavigate } from "react-router-dom"; 
 
+// initial value of input jay
+
 function EditProfile(){
 
     let navigate = useNavigate();
-    let [data ,setData] = useState('')
+    let [data ,setData] = useState('');
+    let [firstName , setFirstName] = useState("jay");
 
     useEffect(()=>{ 
     
@@ -24,14 +27,38 @@ function EditProfile(){
             if(err.response.status == 401 || err.response.status == 498){
                 navigate("/login");
             }
-        }) // path
+        }) 
 
     },[]);
 
     return(
-        <div>
-            {data}
-        </div>
+
+        <main id="editprofile-component">
+            <form id="editprofile-form">
+
+                <div className="profile-pic-div" id='editprofile-image-div'>
+                    <img className="profile-pic" src='images/alexander.jpg'/>
+                </div>    
+
+                <label id="editprofile-profilepic-input-label" htmlFor="editprofile-profilepic-input">change profile picture</label>
+                <input id="editprofile-profilepic-input" type="file"/>
+
+                <label HtmlFor="editprofile-firstname-input">first name</label>
+                <input id="editprofile-firstname-input" type="text"></input>
+
+                <label HtmlFor="editprofile-lastname-input" >last name</label>
+                <input id="editprofile-lastname-input" type="text"></input>
+
+                <label htmlFor='editprofile-bio-input'>bio</label>
+                <input id="editprofile-bio-input" type='textarea'></input>
+
+                <div id="editprofile-submit-btn-wrap">
+                    <button id="editprofile-submit-btn" type="submit">Submit</button>
+                </div>
+
+
+            </form>
+        </main>
     )
 }
 
